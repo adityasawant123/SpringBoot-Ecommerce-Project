bd218296818922d212c195672cd55a16f69e66d3
adityasawant123 <adityasawant132@gmail.com>
2026-08-13 00:50:26 +0530

    Order and Payment Insertion

--- src/main/java/com/ecommerce/project/model/Payment.java
+++ src/main/java/com/ecommerce/project/model/Payment.java
@@ -29,9 +29,9 @@ public class Payment {
     private String pgName;
 
 
-    public Payment(Long paymentId, String pgPaymentId, String pgStatus, String pgResponseMessage,String pgName)
+    public Payment(String paymentMethod, String pgPaymentId, String pgStatus, String pgResponseMessage,String pgName)
     {
-        this.paymentId = paymentId;
+        this.paymentMethod = paymentMethod;
         this.pgPaymentId = pgPaymentId;
         this.pgStatus = pgStatus;
         this.pgResponseMessage = pgResponseMessage;
